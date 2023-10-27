ce6556333884d9d4795c325d03a93ad422202a7b
jeffkf123 <148153706+jeffkf123@users.noreply.github.com>
2023-10-28 04:31:58 +0900

    new files

--- assignment3_template.py
+++ assignment3_template.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
+
 import matplotlib.pyplot as plt
-import lifereader
 import numpy as np
+import lifereader
 import tensorflow as tf
 import math
 import timeit
 
-board = lifereader.readlife('files/BREEDER3.LIF', 2048)
+board = lifereader.readlife('BREEDER3.LIF', 2048)
 
 plt.figure(figsize=(20,20))
 plotstart=924
@@ -17,23 +17,36 @@ plt.figure(figsize=(20,20))
 plt.imshow(board)
 
 #tf.config.set_visible_devices([], 'GPU')
-#tf.debugging.set_log_device_placement(True)
+tf.debugging.set_log_device_placement(True)
 
 boardtf = tf.cast(board, dtype=tf.float16)
 
 @tf.function
 def runlife(board, iters):
-    # Init work
-    
+    # Define the 3x3 filter for convolution to compute the number of neighbors
+    filter = tf.constant([[1, 1, 1],
+                          [1, 0, 1],
+                          [1, 1, 1]], dtype=board.dtype)
+    filter = tf.reshape(filter, [3, 3, 1, 1])
+
     for i in range(iters):
-        # In each iteration, compute two bool tensors
-        # ’survive’ and ’born’: TODO
-        
-        # Then, update the board by keeping these tensors
+        # Convert board to boolean for logical operations
+        bool_board = tf.cast(board, dtype=tf.bool)
+
+        # Compute the number of neighbors for each cell using convolution
+        neighbors = tf.nn.conv2d(tf.reshape(board, [1, board.shape[0], board.shape[1], 1]), filter, strides=[1, 1, 1, 1], padding='SAME')
+        neighbors = tf.squeeze(neighbors)
+
+        # Compute the 'survive' and 'born' tensors based on the game rules
+        survive = tf.logical_and(bool_board, tf.logical_or(tf.equal(neighbors, 2), tf.equal(neighbors, 3)))
+        born = tf.logical_and(tf.logical_not(bool_board), tf.equal(neighbors, 3))
+
+        # Update the board
         board = tf.cast(tf.logical_or(survive, born), board.dtype)
-        
-    # Final work
+
     return board
+
+
 
 
 tic = timeit.default_timer()
